f9f455259957543f9b4407f2dd00261f19a7454c
sohil-khann <khaa40408@gmail.com>
2025-12-18 22:43:41 +0530

    [Sohil khan] Add. Program.cs

---
+++ Level_02/Level_02/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Level_02Programs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Direct calls to program methods. Comment out the ones you don't want to run.
+        LeapYearMultiple.Run();
+        // GradeCalculator.Run();
+        // PrimeCheck.Run();
+        // FizzBuzzFor.Run();
+        // FizzBuzzWhile.Run();
+        // BMI.Run();
+        // Friends.Run();
+        // GreatestFactor.Run();
+        // PowerOfNumber.Run();
+        // Factors.Run();
+        // MultiplesBelow100.Run();
+        Console.ReadKey();
+    }
+}
