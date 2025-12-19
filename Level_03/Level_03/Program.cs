d6565c4fd4ced71bc1a2f45d755d0a83430fa8d8
sohil-khann <khaa40408@gmail.com>
2025-12-19 11:02:15 +0530

    [Sohil khan] Add. Program.cs

---
+++ Level_03/Level_03/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Level_03;
+
+namespace Level_03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // To run a specific exercise, uncomment its Run() call and comment out others.
+            // 1. Armstrong Number
+            Armstrong.Run();
+
+            // 2. Count digits
+            // DigitCount.Run();
+
+            // 3. Harshad Number
+            // Harshad.Run();
+
+            // 4. Abundant Number
+            // Abundant.Run();
+
+            // 5. Day of Week
+            // DayOfWeekProgram.Run();
+
+            // 6. Calculator
+            // Calculator.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
