c682521471078fdf3f5a9353ffabb4bdeab53fc6
sohil-khann <khaa40408@gmail.com>
2025-12-20 11:58:48 +0530

    [Sohil khan] Add. Solution for Program.cs

---
+++ Level_01/Level_01/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        /* CheckVoteEligibility.run(); // Problem 1: Check voting eligibility for 10 ages */
+        //Problem2_PosNegEvenOdd.run(); // Problem 2: Positive/Negative/Zero + Even/Odd and first vs last element
+        // Problem3_MultiplicationTable.run();
+        // Problem4_StoreUpToTenOrUntilNonPositive.run();
+        //     Problem5_MultiplicationTable6to9.run();
+        // roblem6_MeanHeight.run();
+        // Problem7_SaveOddEven.run();
+        //Problem8_FactorsDynamicArray.run();
+        //Problem9_Copy2DTo1D.run();
+        Problem10_FizzBuzz.run();
+
+ Console.ReadKey();
+
+        }
+    }
