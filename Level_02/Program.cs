f8f50ca116f3428f57f43afb0fecf2427124bd1d
sohil-khann <khaa40408@gmail.com>
2025-12-21 12:50:35 +0530

    [sohil khan] Add. Program.cs

---
+++ Level_02/Program.cs
@@ -0,0 +1,55 @@
+﻿
+//// Problem 1: Count Vowels and Consonants
+//VowelConsonantCounter.CountVowelsAndConsonants("Hello World");
+//VowelConsonantCounter.CountVowelsAndConsonants("Programming");
+
+//// Problem 2: Reverse a String
+//StringReverser.DisplayReversedString("Hello");
+//StringReverser.DisplayReversedString("12345");
+
+//// Problem 3: Palindrome Check
+
+//PalindromeChecker.CheckPalindrome("racecar");
+//PalindromeChecker.CheckPalindrome("hello");
+//PalindromeChecker.CheckPalindrome("A man a plan a canal Panama");
+
+//// Problem 4: Remove Duplicates
+
+//DuplicateRemover.DisplayDuplicatesRemoved("mississippi");
+//DuplicateRemover.DisplayDuplicatesRemoved("aabbcc");
+
+//// Problem 5: Find Longest Word
+
+//LongestWordFinder.DisplayLongestWord("The quick brown fox jumps");
+//LongestWordFinder.DisplayLongestWord("Programming is fun and interesting");
+
+//// Problem 6: Substring Occurrences
+//SubstringCounter.DisplaySubstringCount("abcabcabc", "abc");
+//SubstringCounter.DisplaySubstringCount("Hello World Hello", "Hello");
+
+//// Problem 7: Toggle Case
+//CaseToggler.DisplayToggledCase("Hello World");
+//CaseToggler.DisplayToggledCase("PyThOn");
+
+//// Problem 8: Compare Strings Lexicographically
+//LexicographicComparator.CompareTwoStrings("apple", "banana");
+//LexicographicComparator.CompareTwoStrings("zebra", "apple");
+//LexicographicComparator.CompareTwoStrings("cat", "cat");
+
+//// Problem 9: Most Frequent Character
+//FrequentCharacterFinder.DisplayMostFrequentCharacter("success");
+//FrequentCharacterFinder.DisplayMostFrequentCharacter("aabbbbcc");
+
+//// Problem 10: Remove Specific Character
+//SpecificCharacterRemover.DisplayCharacterRemoved("Hello World", 'l');
+//SpecificCharacterRemover.DisplayCharacterRemoved("programming", 'g');
+
+//// Problem 11: Anagram Check
+//AnagramChecker.CheckAnagrams("listen", "silent");
+//AnagramChecker.CheckAnagrams("hello", "world");
+//AnagramChecker.CheckAnagrams("dormitory", "dirty room");
+
+// Problem 12: Replace Word
+WordReplacer.DisplayWordReplaced("Hello World, Hello Everyone", "Hello", "Hi");
+WordReplacer.DisplayWordReplaced("The cat and the dog", "the", "a");
+Console.ReadKey();
