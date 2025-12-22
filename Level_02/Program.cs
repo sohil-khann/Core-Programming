8394b454c9d0e9985409093b868412f8d8715654
sohil-khann <khaa40408@gmail.com>
2025-12-22 11:25:30 +0530

    [Sohil khan] Add. Program.cs

---
+++ Level_02/Program.cs
@@ -0,0 +1,23 @@
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //FactorCalc.runMethod();
+        //CompareSumOfNum.runMethod();
+        //CheckLeapYear.runMethod();
+        //StudentVoteChecker.runMethod();
+        //FriendStats.runMethod();
+        //NumberAnalyzer.runMethod();
+        //BMICalculator.runMethod();
+        //QuadraticEquation.runMethod();
+        //RandomArrayStats.runMethod();
+        
+        //Console.WriteLine( UnitConverter.ConvertKmToMiles(10)); // Example usage
+        //Console.WriteLine(UnitConverterExtended.ConvertYardsToFeet(105));
+        //Console.WriteLine(UnitConverterFinal.ConvertFahrenheitToCelsius(98.6));
+
+        Console.ReadKey();
+    }
+}
