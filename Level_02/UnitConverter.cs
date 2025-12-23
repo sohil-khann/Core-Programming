9389e4f4c17a73222475f42a5d3a7845b29751c6
sohil-khann <khaa40408@gmail.com>
2025-12-23 10:53:22 +0530

    [Sohil khan] Update.

--- Level_02/UnitConverter.cs
+++ Level_02/UnitConverter.cs
@@ -8,4 +8,30 @@ code double miles2km = 1.60934;
 c. Method to convert meters to feet and return the value. Use the following code
 to convert double meters2feet = 3.28084;
 d. Method to convert feet to meters and return the value. Use the following code
-to convert double feet2meters = 0.3048;*/using System;internal static class UnitConverter{    private const double KmToMilesFactor = 0.621371;    private const double MilesToKmFactor = 1.60934;    private const double MetersToFeetFactor = 3.28084;    private const double FeetToMetersFactor = 0.3048;      public static double ConvertKmToMiles(double km)    {        return km * KmToMilesFactor;    }    public static double ConvertMilesToKm(double miles)    {        return miles * MilesToKmFactor;    }    public static double ConvertMetersToFeet(double meters)    {        return meters * MetersToFeetFactor;    }    public static double ConvertFeetToMeters(double feet)    {        return feet * FeetToMetersFactor;    }}
+to convert double feet2meters = 0.3048;
+*/
+using System;
+internal static class UnitConverter
+{
+    private const double KmToMilesFactor = 0.621371;
+    private const double MilesToKmFactor = 1.60934;
+    private const double MetersToFeetFactor = 3.28084;
+    private const double FeetToMetersFactor = 0.3048;
+  
+    public static double ConvertKmToMiles(double km)
+    {
+        return km * KmToMilesFactor;
+    }
+    public static double ConvertMilesToKm(double miles)
+    {
+        return miles * MilesToKmFactor;
+    }
+    public static double ConvertMetersToFeet(double meters)
+    {
+        return meters * MetersToFeetFactor;
+    }
+    public static double ConvertFeetToMeters(double feet)
+    {
+        return feet * FeetToMetersFactor;
+    }
+}
